b6a40b10111be3a161d7654dceef9e8bc9e51734
kokinz <nes-t@mail.ru>
2021-08-22 00:25:19 +0300

    history

--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Promo from '../promo/promo';
 import Converter from '../converter/converter';
+import History from '../history/history';
 
 function Main() {
   return(
@@ -9,35 +10,7 @@ function Main() {
 
       <Converter />
 
-      <section className="history">
-        <h2 className="history__header">
-          История конвертация
-        </h2>
-
-        <ul className="history__list">
-          <li className="history__item">
-            <span className="history__date">
-              25.11.2020
-            </span>
-            <span className="history__first-value">
-              1000
-            </span>
-            <span className="history__first-currency">
-              RUB
-            </span>
-            <span className="history__second-value">
-              13,1234
-            </span>
-            <span className="history__second-currency">
-              USD
-            </span>
-          </li>
-        </ul>
-
-        <button className="history__button button">
-          Очистить историю
-        </button>
-      </section>
+      <History />
     </main>
   );
 }
